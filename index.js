90c8f608a2ba0476e07483454d46e89c20190ee3
André Santos <andrefs@cpan.org>
2015-06-26 01:14:31 +0100

    Updates server port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var fs = require('fs');
 var index = fs.readFileSync('cv_andrefs.pdf');
-var port = 9615;
+var port = 5001;
 
 console.log('Listening on http://localhost:'+port);
 http.createServer(function (req, res) {
